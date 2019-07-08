2904d699553470b4f41e055e50ec4401e942689c
daniela.goel <danigoel@gmail.com>
2019-07-08 20:07:47 +0300

    new file:   String.h

---
+++ c/String.h
@@ -0,0 +1,15 @@
+/*STRING LIB RE-IMPLEMENTATIONS:*/
+size_t MyStrlen(char *str);
+void StrlenTest();
+int MyStrcmp(char *str_1, char *str_2);
+void StrcmpTest();
+char* MyStrcpy(char *dest, char *source);
+void StrcpyTest();
+char *MyStrchr(const char *str, int character);
+/*MyStrchr test function here*/
+char *MyStrdup(const char *string);
+/*MyStrdup test function here*/
+
+/*OTHER EXERCISES:*/
+int IsPalindrome(const char *str);
+void IsPalindromeTest();
