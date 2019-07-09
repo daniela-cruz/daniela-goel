f5eab1efd90648fa37426d92594c7e25954536e3
daniela.goel <danigoel@gmail.com>
2019-07-09 11:56:05 +0300

    modified:   String.c

--- c/String.c
+++ c/String.c
@@ -1,3 +1,26 @@
+#include <stdio.h> /*standard input-output*/
+#include <stdlib.h> /*basic functions, allocations, null etc.*/
+#include <string.h> /*string functions*/
+
+#include "String.h" /*This file's declarations*/
+
+#define SIZE 4
+
+void DerUltimateTester();
+void StrlenTest();
+void StrcmpTest();
+void StrcpyTest();
+char *MyStrchr(const char *str, int character);
+/*MyStrchr test function here*/
+char *MyStrdup(const char *string);
+/*MyStrdup test function here*/
+
+int main()
+{
+	DerUltimateTester();
+	
+	return 0;
+}
 
 /*===EXERCISES:===*/
 /*STRING.H RE-IMPLEMENTATION:*/
@@ -37,6 +60,7 @@ char* MyStrcpy(char *dest, char *source)
 	return dest;
 }
 
+/*No tests yet:*/
 char *MyStrchr(const char *str, int character)
 {	
 	while ((*str != '\0') && (*str != character))
@@ -44,15 +68,15 @@ char *MyStrchr(const char *str, int character)
 		str++;
 	}
 	
-	return	(*string == character) ? (char *) s : NULL;
+	return	(*str == character) ? (char *) str : NULL;
 }
 
 char *MyStrdup(const char *string)
 {
 	int size = strlen(string);
-	char *p = *malloc(size * sizeof(char));
+	char *p = (char *)malloc(size * sizeof(char));
 	
-	while (*stirng != '\0')
+	while (*string != '\0')
 	{
 		*p = *string;
 		p++;
@@ -62,30 +86,20 @@ char *MyStrdup(const char *string)
 	return p;
 }
 
-
-
-/*Other exercises:*/
-int IsPalindrome(const char *str)
-{
-	int size = strlen(str), flag = -1, i;
-
-	for (i=0; i<size/2; i++)
-	{
-		if (str[i] == str[size-i-1])
-		{
-			flag = 0;
-		}
-		else
-		{
-			break;
-		}
-	}
-	
-	return flag;
-}
-
 /*===TESTS:===*/
-void StrlenTest()
+void DerUltimateTester()
+{
+	int size = SIZE, i = 0;
+	/*int compared[] = {32, 32, 0, -32};
+	int *com = compared;*/
+	char *p_arr[] = {"word", "Hello", "hello", "hello"};
+	char *p_cmp[] = {"woRd", "HellO", "hello", "hello "};
+	
+	StrlenTest();
+	
+	
+}
+void StrlenTest() /*MORE CASES NEEDED!! I need to create an array of stirngs!*/
 {
 	char str[] = "word";
 	printf("I gots da %ld characters, yas!\n", MyStrlen(str));
@@ -93,31 +107,35 @@ void StrlenTest()
 
 void StrcmpTest()
 {
-	char *p_1 = "word", *p_2 = "woRd"; 
-	int compared = MyStrcmp(p_1, p_2);
-	
-	printf("The result of comparison is: %d\n", compared);
+	int size = SIZE, i = 0;
+	int compared[] = {32, 32, 0, -32};
+	int *com = compared;
+	char *p_arr[] = {"word", "Hello", "hello", "hello"};
+	char *p_cmp[] = {"woRd", "HellO", "hello", "hello "};
+	char **p1 = p_arr, **p2 = p_cmp; /*the content of the p_ arrays is an array //
+										of pointers*/
+
+	for (i = 0; i < size; i++)
+	{
+		*p1 = *p_arr[i] ;
+		*p2 = *p_cmp[i];
+		while (size > 0)
+		{
+			assert (MyStrcmp(p1, p2) == compared);
+			p1++;
+			p2++;
+			com++;
+			size--;
+		}
+	}
 }
 
 void StrcpyTest()
 {
-	char str[] = "word", location[] = {'0', '0', '0', '0', '0', '0', '0', '\0'};
+	char const str[] = "word", 
+	char location[] = {'0', '0', '0', '0', '0', '0', '0', '\0'};
+	char *p = MyStrcpy(location, str);
 	
-	MyStrcpy(location, str);
-}
-
-void IsPalindromeTest()
-{
-	char word_arr[] = {'m', 'a', 'a', 'm', '\0'};
-	char *str = word_arr; 
-	
-	if(IsPalindrome(str) == 0)
-	{
-		printf("Yasss!\n");
-	}
-	else
-	{
-		printf("Ney!\n");
-	}
+	assert ();
 }
 
