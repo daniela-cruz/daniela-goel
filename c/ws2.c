33f45f8f32dedd8469fc86ba4ed1f6b1a612c4a3
daniela.goel <danigoel@gmail.com>
2019-07-09 11:55:47 +0300

    modified:   ws2.c

--- c/ws2.c
+++ c/ws2.c
@@ -2,12 +2,49 @@
 #include <stdlib.h> /*basic functions, allocations, null etc.*/
 #include <string.h> /*string functions*/
 
+int IsPalindrome(const char *str);
+void IsPalindromeTest();
+
 int main()
-{
-	StrlenTest();
-	StrcpyTest();
-	StrcmpTest();
+{	
+	IsPalindromeTest();
 	
 	return 0;
+}
+
+
+/*Other exercises:*/
+int IsPalindrome(const char *str)
+{
+	int size = strlen(str), flag = -1, i;
+
+	for (i=0; i<size/2; i++)
+	{
+		if (str[i] == str[size-i-1])
+		{
+			flag = 0;
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	return flag;
+}
+
+void IsPalindromeTest() /*REQUIRES TEST!!!*/
+{
+	char word_arr[] = {'m', 'a', 'a', 'm', '\0'};
+	char *str = word_arr; 
+	
+	if(IsPalindrome(str) == 0)
+	{
+		printf("Yasss!\n");
+	}
+	else
+	{
+		printf("Ney!\n");
+	}
 }
 
