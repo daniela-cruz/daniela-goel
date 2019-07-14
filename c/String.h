8cd3dc42075463f5e662109f75b1828bcf4830c2
daniela.goel <danigoel@gmail.com>
2019-07-14 08:40:08 +0300

    modified:   String.h

--- c/String.h
+++ c/String.h
@@ -1,13 +1,22 @@
 /*STRING LIB RE-IMPLEMENTATIONS:*/
-size_t MyStrlen(char *str);
+size_t MyStrlen(const char *str);
 
-int MyStrcmp(char *str_1, char *str_2);
+int MyStrcmp(const char *str_1, const char *str_2);
 
-char* MyStrcpy(char *dest, char *source);
+int MyStrcasecmp(const char *source, const char *string);
+
+char* MyStrcpy(char *dest, const char *src);
+
+char *MyStrncpy(char *dest, const char *source, size_t size);
 
 char *MyStrchr(const char *str, int character);
 
 char *MyStrdup(const char *string);
 
+char *MyStrcat(char *dest, const char *src);
+
+char *MyStrncat(char *dest, const char *src, size_t size);
+
+char *MyStrstr(const char *haystack, const char *needle);
 
 
