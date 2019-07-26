ac33e06d08be395ea50bf1184fb5a178694ec524
daniela.goel <danigoel@gmail.com>
2019-07-26 17:26:02 +0300

    modified:   ws8.c

--- c/ws8.c
+++ c/ws8.c
@@ -2,6 +2,8 @@
 #include <stdlib.h> /*printf*/
 #include <string.h> /*strcat*/
 
+#define DUMMY_VALUE 10
+
 typedef struct add_data 
 { 
 	void *data();
@@ -10,6 +12,14 @@ typedef struct add_data
 	void (*free_memory)(void);
 } Add_Data;
 
+int *AddInt(int *number);
+float *AddFloat(float *number);
+
+void PrintInt(int  *number);
+void PrintFloat(float  *number);
+void PrintString(char  *string);
+
+FreeString;
 
 int main() 
 {
@@ -19,17 +29,46 @@ int main()
 Add_Data *InitializeDAtaStruct(char struct_elements_number) 
 {
     Add_Data type_functions = 
-					{{(int)number, (int)AddInt, PrintInt, FreeNumber}, 
-					{(char *)string, (char *)strcat, PrintString, FreeString},
-					{(float)number, (float *)AddFloat, PrintFloat, FreeNumber}};
+					{{(int *)number, (int *)AddInt, PrintInt, FreeNumber}, 
+					{(float *)number, (float *)AddFloat, PrintFloat, FreeNumber},
+					{(char *)string, (char *)strcat, PrintString, FreeString}};
     
 
     return type_functions;
 }
 
-
-int AddInt(int number)
+/*Addition functions:*/
+int *AddInt(int *number)
 {
+	*number += DUMMY_VALUE;
 	
-	return number;	
+	return  number;	
+}
+
+float *AddFloat(float *number)
+{
+	*number += DUMMY_VALUE;
+	
+	return  number;	
+}
+
+/*Print functions:*/
+void PrintInt(int  *number)
+{
+	printf("%d", *number);
+}
+
+void PrintFloat(float  *number)
+{
+	printf("%f", *number);
+}
+
+void PrintString(char  *string)
+{
+	printf("%s", string);
+}
+
+FreeString
+{
+	free(string);
 }
