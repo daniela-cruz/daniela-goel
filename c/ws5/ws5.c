9bcb9c9c86cd123fd2adcb8dd9a9342e7fd352fd
daniela.goel <danigoel@gmail.com>
2019-09-09 19:19:46 +0300

    new file:   ws5.c

---
+++ c/ws5/ws5.c
@@ -0,0 +1,14 @@
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* escape */
+#include <string.h> /* getchar */
+
+typedef enum log_func_t {APPEND, TOP, COUNT, REMOVE, EXIT}
+
+
+
+int main()
+{
+
+	return 0;
+}
+
