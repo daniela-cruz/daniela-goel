7d284684e9427b8dd2aa4e36d48eaf4d206ad826
daniela.goel <danigoel@gmail.com>
2019-09-10 08:48:04 +0300

    modified:   ws5.c

--- c/ws5/ws5.c
+++ c/ws5/ws5.c
@@ -2,13 +2,45 @@
 #include <stdlib.h> /* escape */
 #include <string.h> /* getchar */
 
-typedef enum log_func_t {APPEND, TOP, COUNT, REMOVE, EXIT}
+enum operations {TOP, COUNT, REMOVE, EXIT, APPEND}
 
+struct log_module_t 	
+{	
+	char *user_input;
+	enum function_module(char *user_input, FILE *file_name);
+};
 
+void Logger();
 
 int main()
 {
-
+	Logger();
+	
 	return 0;
+}
+
+void Logger()
+{
+	char *str = NULL;
+	FILE *file_name = NULL;
+	struct log_module_t[] = 	{{"<", AddToTop},
+										{"-count", CountLines},
+										{"-remove", RemoveFIle},
+										{"-exit", Escape},
+										{"", Append}};
+	enum i = 0;
+	
+	while (1)
+	{
+		Printf("Please enter your string\n");
+		str = fgets();
+		
+		for (; i < APPEND; i++)
+	{
+		if (str == log_module_t[i].user_input)
+		{
+			log_module_t[i].function_module(str, file_name);
+		}
+	}
 }
 
