c0df6270310f472cc4823eda4074f9849850cd8d
daniela.goel <danigoel@gmail.com>
2019-09-10 12:27:25 +0300

    modified:   ws5.c

--- c/ws5/ws5.c
+++ c/ws5/ws5.c
@@ -1,16 +1,25 @@
+#ifndef NDEBUG
+#define DEBUG_PRINT(s) printf(s)
+#endif
+
 #include <stdio.h> /* printf */
 #include <stdlib.h> /* escape */
 #include <string.h> /* getchar */
 
-enum operations {TOP, COUNT, REMOVE, EXIT, APPEND}
+#define MODULES_SIZE 2
+
+enum status_t {SUCCESS, ERR_FOPEN};
 
 struct log_module_t 	
 {	
 	char *user_input;
-	enum function_module(char *user_input, FILE *file_name);
+	enum status_t (*function_module)(const char *user_input, const char *file_name);
 };
 
 void Logger();
+
+enum status_t Append(const char *user_input, const char *file_name);
+enum status_t EscapeFunction(const char *user_input, const char *file_name);
 
 int main()
 {
@@ -22,25 +31,54 @@ int main()
 void Logger()
 {
 	char *str = NULL;
-	FILE *file_name = NULL;
-	struct log_module_t[] = 	{{"<", AddToTop},
-										{"-count", CountLines},
-										{"-remove", RemoveFIle},
-										{"-exit", Escape},
-										{"", Append}};
-	enum i = 0;
+	char *file_name = NULL;
+	struct log_module_t operations[] = {	/*{"<", AddToTop},
+														{"-count", CountLines},
+														{"-remove", RemoveFIle},*/
+														{"-exit", EscapeFunction},
+														{"", Append}};
+	int i = 0;
+	
+	printf("Give your file a name:\n");
+	gets(file_name);
 	
 	while (1)
 	{
 		Printf("Please enter your string\n");
-		str = fgets();
+		gets(str);
 		
-		for (; i < APPEND; i++)
+		for (; i < MODULES_SIZE; i++)
 	{
-		if (str == log_module_t[i].user_input)
+		if (0 == strcmp(str, operations[i].user_input))
 		{
-			log_module_t[i].function_module(str, file_name);
+			operations[i].function_module(str, file_name);
 		}
 	}
 }
 
+enum status_t Append(const char *user_input, const char *file_name)
+{
+	FILE *file = NULL;
+	
+	/*DEBUG_PRINT("Append parsing succeeded!\n")*/
+	
+	if (NULL == (file = fopen(file_name, "a+")))
+	{
+		fprintf (stderr, "%s: Couldn't open file %s.\n", file_name);
+		return ERR_FOPEN;
+	}
+	
+	fprintf(file, "%s", user_input);
+	
+	fclose(file_name);
+	
+	return SUCCESS;
+}
+
+enum status_t EscapeFunction(const char *user_input, const char *file_name)
+{
+	printf("Now it's time to die! G'bye!\n");
+	system("stty icanon echo");
+	
+	exit(0);
+}
