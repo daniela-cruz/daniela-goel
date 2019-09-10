810bfa9f46677b9a3946af1a63a14fac2d1749bc
daniela.goel <danigoel@gmail.com>
2019-09-10 19:03:27 +0300

    modified:   ws5.c

--- c/ws5/ws5.c
+++ c/ws5/ws5.c
@@ -5,25 +5,30 @@
 #include <stdio.h> /* printf */
 #include <stdlib.h> /* escape */
 #include <string.h> /* getchar */
+#include <assert.h> /* assert */
 
-#define MODULES_SIZE 2
+#define MODULES_SIZE 4
 #define STR_MAX 100
 
-enum status_t {SUCCESS, FAILURE};
+typedef enum status {SUCCESS, FAILURE} status_t;
 
 struct operation 	
 {	
 	char *action_request;
-	enum status_t (*action)(const char *, const char *);
+	status_t (*action)(const char *, const char *);
 };
 
 static void Logger();
-static enum status_t Parser(const char *user_input, const char* action_req, size_t index);
+static status_t Parser(const char *user_input, const char* path);
 
-static enum status_t Append(const char *user_input, const char *path);
-static enum status_t TrueFunction();
+static status_t EscapeFunction(const char *user_input, const char *path);
 
-static enum status_t EscapeFunction(const char *user_input, const char *path);
+static status_t Append(const char *user_input, const char *path);
+static status_t TrueFunction();
+
+static status_t PushToTop(const char *user_input, const char *path);
+
+static status_t RemoveFile(const char *user_input, const char *path);
 
 int main(int argc, char **argv)
 {
@@ -37,57 +42,154 @@ void Logger(char *path_input)
 {
 	char user_input[STR_MAX] = {0};
 	char *path = path_input;
-	struct operation operations[] = {	/*{"<", AddToTop},
-														{"-count", CountLines},
-														{"-remove", RemoveFIle},*/
+	size_t i = 0;
+	size_t compare = 0;
+	status_t status = SUCCESS;
+	struct operation operations[] = {	{"<", PushToTop},
+														/*{"-count", CountLines},*/
+														{"-remove", RemoveFile},
 														{"-exit", EscapeFunction},
 														{"", Append}};
-	size_t i = 0;
 	
-	while (SUCCESS == operations[i].action(user_input, path))
+	while (FAILURE != Parser(user_input, path))
 	{
 		printf("Please enter your string\n");
 		fgets(user_input, STR_MAX, stdin);
-		
-		for (; i < MODULES_SIZE; i++)
+		/*
+		for(; i < )
 		{
-			if (SUCCESS == Parser(user_input, operations[i].action_request, i))
-			{
-				operations[i].action(user_input, path);
-				break;
-			}
-		}
+		
+		}*/
 	}
 }
 
-enum status_t Parser(const char *user_input, const char* action_req, size_t index)
+status_t Parser(const char *user_input, const char* path)
 {
-	enum status_t status = SUCCESS;
+	size_t i = 0;
+	size_t compare = 0;
+	char ch = 0;
+	status_t status = SUCCESS;
+	struct operation operations[] = {	{"<", PushToTop},
+														/*{"-count", CountLines},*/
+														{"-remove", RemoveFile},
+														{"-exit", EscapeFunction},
+														{"", Append}};
 	
 	printf("I am the PARSER!\n");
 	
-	if (0 == index)
-	{
-		status = SUCCESS;
-	}
-	else if (index == 1)
-	{
-		status = TrueFunction();
-	}
-	else
-	{
-		status = FAILURE;
-	}
+	ch = *user_input;
 	
+	switch(ch)
+	{
+		case '<':
+			status = PushToTop(user_input, path);
+			break;
+		
+		case '-':
+			{
+				compare = strlen(user_input);
+				(0 == strncmp(user_input, "-exit", compare)) ? status = EscapeFunction(user_input, path): status;
+				
+			}
+			break;
+		
+		default:
+			status = Append(user_input, path);
+			break;
+	
+	}
+		/*
+		if ('<' == *user_input)
+		{
+			PushToTop(user_input, path);
+		}
+		else if (strlen(operations[i].action_request) + 1 == strlen(user_input))
+		{
+			printf("I am the first exit condition!\n");
+			
+			if (0 == strncmp(user_input, operations[i].action_request, compare))
+			{
+				printf("I am the second exit condition!\n");
+				EscapeFunction(user_input, path);
+			}
+			
+			break;
+		}
+		else if (user_input)
+		{
+			Append(user_input, path);
+			break;
+		}
+		else
+		{
+			status = FAILURE;
+		
+	}*/
+
 	return status;
 }
 
-enum status_t Append(const char *user_input, const char *path)
+static status_t PushToTop(const char *user_input, const char *path)
 {
+	FILE *temp_file = NULL;
 	FILE *file = NULL;
+	char ch = 0;
+	
+	assert(NULL != user_input);
+	user_input++;
+	file = fopen(path, "a+");
+	temp_file = fopen("temp_file", "w");
+	
+	if (path == NULL)
+	{
+	  printf("Error! Can't open file!\n");
+	  exit(1);
+	}
+	
+	/*go to the beginnig of the file and send back a pointer*/	
+	Append(user_input, "temp_file");
+	temp_file = fopen("temp_file", "a+");
+	
+	while(1)
+	{
+		ch = fgetc(file);
+
+		if (ch == EOF)
+		{
+			break;
+		}
+		putc(ch, temp_file);
+	} 
+
+	RemoveFile("-remove", path);
+	rename("temp_file", path);
+	fclose(temp_file);
+	
+	return SUCCESS;
+}
+
+status_t RemoveFile(const char *user_input, const char *path)
+{
+	int status = remove(path);
+	
+	if (status == 0)
+	{
+		printf("%s file deleted successfully.\n", path);
+	}
+	else
+	{
+		printf("Unable to delete the file\n");
+		/*ERROR NEEDED HERE*/
+	}
+	
+	return SUCCESS;
+}
+
+status_t Append(const char *user_input, const char *path)
+{
+	FILE *file = fopen(path, "a+");
 	
 	printf("I am the APPENDER!!\n");
-	file = fopen(path, "a+");
 	
 	if (NULL == file)
 	{
@@ -101,14 +203,15 @@ enum status_t Append(const char *user_input, const char *path)
 	return SUCCESS;
 }
 
-enum status_t TrueFunction()
+status_t TrueFunction()
 {
 	return SUCCESS;
 }
 
-enum status_t EscapeFunction(const char *user_input, const char *path)
+status_t EscapeFunction(const char *user_input, const char *path)
 {
 	printf("Now it's time to die! G'bye!\n");
+	/*exit(0);*/
 	
-	exit(0);
+	return FAILURE;
 } 
