4c9babdc73e136f47216a4ed0878e9b1bf06ac78
daniela.goel <danigoel@gmail.com>
2019-09-11 18:27:28 +0300

    modified:   ws6.c

--- c/ws6/ws6.c
+++ c/ws6/ws6.c
@@ -4,12 +4,14 @@
 #define BASE 2
 
 static long Pow2(unsigned int x, unsigned y);
+static void Pow2Test();
+
 static size_t IsPowerOf2 (unsigned int n);
 static void IsPowerOf2Test();
 
 int main()
 {
-	/* test */
+	Pow2Test();
 	IsPowerOf2Test();
 	
 	return 0;
@@ -18,6 +20,34 @@ int main()
 static long Pow2(unsigned int x, unsigned int y)
 {
 	return x << y;
+}
+
+static void Pow2Test()
+{
+	long expected_results[4][4] = {	{0, 0, 0, 0},
+												{1, 2, 4, 8}, 
+												{2, 4, 8, 16}, 
+												{3, 6, 12, 24}};
+	size_t x[] = {0, 1, 2, 3};
+	size_t y[] = {0, 1, 2, 3};
+	size_t i = 0, j = 0;
+	
+	printf("\nPow2 test:\n");
+	
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			if (expected_results[i][j] == Pow2(x[i], y[j]))
+			{
+				printf("SUCCESS!!\n");
+			}
+			else
+			{
+				printf("Failed test. Returned value is %d\n");
+			}
+		}
+	}
 }
 
 static size_t IsPowerOf2 (unsigned int n)
