f9d0391b39d0931791f57616063e22ea0b2a21c5
daniela.goel <danigoel@gmail.com>
2019-09-12 15:38:18 +0300

    modified:   ws6.c

--- c/ws6/ws6.c
+++ c/ws6/ws6.c
@@ -2,17 +2,33 @@
 #include <assert.h> /* assert */
 
 #define BASE 2
+#define BITS_IN_BYTE sizeof(size_t) * 8
+
 
 static long Pow2(unsigned int x, unsigned y);
 static void Pow2Test();
 
 static size_t IsPowerOf2 (unsigned int n);
+static size_t IsOowerOf2NoLoop(unsigned int n);
 static void IsPowerOf2Test();
+
+static size_t AddBitwise(size_t num);
+static void AddBitwiseTest();
+
+static void FindNumsWith3BitsOn(size_t num_arr[], size_t arr_size);
+static size_t CountBits(size_t num);
+static void FindNumsWith3BitsOnTest();
+
+static unsigned int MirrorLoopBitwise(unsigned int binary_number);
+static void TestMirrorNumberLoop();
 
 int main()
 {
 	Pow2Test();
 	IsPowerOf2Test();
+	AddBitwiseTest();
+	FindNumsWith3BitsOnTest();
+	TestMirrorNumberLoop();
 	
 	return 0;
 }
@@ -73,4 +89,121 @@ static size_t IsPowerOf2 (unsigned int n)
 static void IsPowerOf2Test()
 {
 	printf("Is 7 power of 2? 0 = no, 1 = yes. Answer: %ld\n", IsPowerOf2(7));
+	printf("Is 7 power of 2? 0 = no, 1 = yes. Answer: %ld\n", IsOowerOf2NoLoop(8));
+}
+
+static size_t IsOowerOf2NoLoop(unsigned int n)
+{
+	return (((n - 1) & n) == 0) ? 1:0;
+}
+
+static size_t AddBitwise(size_t num)
+{
+	size_t mask = 0xFFFF;
+	size_t lsb_mask = 0x01;
+	
+	while (0 != (num & lsb_mask))
+	{
+		mask <<= 1;
+		lsb_mask <<= 1;
+	}
+	
+	return (num & mask) | lsb_mask;
+}
+
+static void AddBitwiseTest()
+{
+	size_t num = 27;
+	size_t expected_result = 28;
+	
+	
+	if (expected_result == AddBitwise(num))
+	{
+		printf("SUCCESS!!\n");
+	}
+	else
+	{
+		printf("Failure. Result is: %ld but expected is: %ld\n", AddBitwise(num), expected_result);
+	}
+}
+
+static void FindNumsWith3BitsOn(size_t num_arr[], size_t arr_size)
+{
+	size_t num_ptr = 0;
+	size_t i = 0;
+	const size_t bit_limit = 3;
+	size_t mask = 1;
+	
+	assert(num_arr);
+	
+	for (; i < arr_size; i++)
+	{
+		num_ptr = num_arr[i];
+		if (bit_limit == CountBits(num_arr[i]))
+		{
+			printf("The number %ld has exactly %ld bits on.\n", num_ptr, bit_limit);
+		}
+	}
+}
+
+static size_t CountBits(size_t num)
+{
+	size_t bit_counter = 0;
+	size_t mask = 1;
+	
+	while (0 < num)
+	{
+		if (1 == (num & mask))
+		{
+			bit_counter++;
+		}
+		
+		num >>= 1;
+	}
+	
+	return bit_counter;
+}
+
+static void FindNumsWith3BitsOnTest()
+{
+	size_t num_arr[] = {0, 1, 7, 22, 201};
+	size_t arr_size = 5;
+	
+	FindNumsWith3BitsOn(num_arr, arr_size);
+}
+
+static unsigned int MirrorLoopBitwise(unsigned int binary_number)
+{
+	unsigned char number_of_system_bits = BITS_IN_BYTE; 
+    unsigned int mirrored_number = 0; 
+    unsigned char i = 0; /*also used as a moving mask*/
+     
+    for (; i < number_of_system_bits; i++) 
+    { 
+    	/* 
+    	If the original number AND'ed with mask moved by i bits left:
+    	OR mirrored number with moving mask*/
+        if ((binary_number & (1 << i))) 
+        {
+	        mirrored_number |= 1 << ((number_of_system_bits - 1) - i);
+        }
+   } 
+   
+    return mirrored_number; 
+}
+
+static void TestMirrorNumberLoop()
+{
+	unsigned int numbers[] = {128, 0x01, 0x02, 0x09, 0x07, 0x10};	
+	size_t numbers_array_size = 6;
+	size_t i = 0;
+	
+	for (; i < numbers_array_size; i++)
+	{
+		size_t mirrored = MirrorLoopBitwise(numbers[i]);
+		printf("Index is: %u\n", i);
+		printf("Unmirror output is: %u\n", mirrored);
+		assert(numbers[i] == MirrorLoopBitwise(mirrored));
+		printf("SUCCESS!\n");
+	}
 }
