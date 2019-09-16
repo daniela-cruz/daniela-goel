c6c0c7a1e2ea8ac9f3a309cf6a8d3c4ce067d114
daniela.goel <danigoel@gmail.com>
2019-09-16 10:55:54 +0300

    modified:   ws8.c 	modified:   ws8.h

--- c/ws8/ws8.c
+++ c/ws8/ws8.c
@@ -1,4 +1,5 @@
-#include <>
+#include <stdio.h> /* printf */
+#include <assert.h> /* assert */
 
 typedef enum {INT, FLOAT, STRING} data_type_t;
 
@@ -17,28 +18,90 @@ typedef struct
 
 void SetVal(data_t *arr, data_type_t type /* data_t.dt */, void *data /* data_t.d */)
 {
-	if (NULL == arr_ptr)
-	{
-		return NULL;
-	}
+	assert(arr);
 	
 	arr_ptr->dt = type;
 	
-	switch(type)
+	switch (type)
+	{
 		case INT:
-			(void *)&arr_ptr->d.i_var = data;
+			(void *)&arr_ptr->d.i_var = (int)data;
 			break;
+			
 		case FLOAT:
-			(void *)&arr_ptr->d.f_var = data;
+			(void *)&arr_ptr->d.f_var = (float)data;
 			break;
+			
 		case STRING:
-			(void *)&arr_ptr->d.s_var = data;
+			(void *)&arr_ptr->d.s_var = (char *)data;
 			break;
+			
 		default:
 			break;
+	}
 }
 
-void *GetVal(data_t *arr, size_t index)
+void *GetVal(data_t *arr_ptr)
 {
-	return arr.d;
+	assert(arr);
+	
+	data_t type = arr_ptr->dt;
+	
+	switch (type)
+	{
+		case INT:
+			return (void*)&arr_ptr->d.i_var;
+		
+		case FLOAT:
+			return (void*)&arr_ptr->d.f_var;
+		
+		case STRING:
+			return (void*)&arr_ptr->d.s_var;
+	}
+	
+}
+
+void PrintVal(data_t *arr_ptr)
+{
+	data_t type = arr_ptr->dt;
+	
+	switch (type)
+	{
+		case INT:
+			printf("%d\n", (int)&arr_ptr->d.i_var);
+			break;
+		
+		case FLOAT:
+			printf("%lf\n", (float)&arr_ptr->d.f_var);
+			break;
+		
+		case STRING:
+			printf("%s\n", (char *)&arr_ptr->d.i_var);
+			break;
+		
+		default:
+			printf("Error: no value can be printed.\n");
+			break;
+	}
+}
+
+void *AddVal(data_t *arr_ptr, void *add_value)
+{
+	data_t type = arr_ptr->dt;
+	
+	switch (type)
+	{
+		case INT:
+			return ((int)&arr_ptr->d.i_var + (int)add_value);
+		
+		case FLOAT:
+			return ((float)&arr_ptr->d.f_var + (float)add_value);
+		
+		case STRING:
+			return strcat((char *)&arr_ptr->d.s_var, (char *)add_value);
+		
+		default:
+			printf("Error: cannot perform addition.\n");
+			break;
+	}
 }
--- c/ws8/ws8.h
+++ c/ws8/ws8.h
@@ -1,2 +1,8 @@
 
-void SetVal(data_t *arr, data_type_t type /* data_t.dt */, void *data /* data_t.d */);
+extern void SetVal(data_t *arr, data_type_t type /* data_t.dt */, void *data /* data_t.d */);
+
+extern void *GetVal(data_t *arr_ptr);
+
+extern void PrintVal(data_t *arr_ptr);
+
+extern void *AddVal(data_t *arr_ptr, void *add_value);
