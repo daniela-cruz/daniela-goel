b46d5195b49fa4e7b8ec98e9b894af001891456e
daniela.goel <danigoel@gmail.com>
2019-09-22 13:47:13 +0300

    modified:   BitArray.h

--- ds/src/BitArray.h
+++ ds/src/BitArray.h
@@ -1,16 +1,13 @@
 #ifndef __BIT_ARRAY_H__
 #define __BIT_ARRAY_H__
 
-typedef struct 
-{
-	long bit_arr; /* an array of 64 bits*/
-} bit_arr_t;
+typedef int bit_arr_t; /* an array of 64 bits*/
 
-/* is a particular bit set on */
-extern int IsOn( bit_arr_t *word, size_t bit_location);
+/* 1 if a particular bit set on else 0 */
+extern int IsOn(const bit_arr_t *arr, size_t bit_location);
 
-/* is a particular bit set off */
-extern int IsOff( bit_arr_t *word, size_t bit_location);
+/* 1 if a particular bit set off else 0 */
+extern int IsOff(const bit_arr_t *arr, size_t bit_location);
 
 /* set on all bits in array*/
 extern void SetAll(bit_arr_t *arr);
@@ -25,18 +22,18 @@ extern bit_arr_t *RotL(bit_arr_t *arr, size_t n);
 extern bit_arr_t *RotR(bit_arr_t *arr, size_t n);
 
 /* flip a single bit */
-extern bit_arr_t *Flip(bit_arr_t *word, size_t bit_location);
+extern bit_arr_t *Flip(bit_arr_t *arr, size_t bit_location);
 
 /* count number of set bits */
-extern size_t CountOn(bit_arr_t *arr);
+extern size_t CountOn(const bit_arr_t *arr);
 
 /* count number of off bits */
-extern size_t CountOff(bit_arr_t *arr);
+extern size_t CountOff(const bit_arr_t *arr);
 
 /* mirror bit array */
 extern bit_arr_t *Mirror(bit_arr_t *arr);
 
 /* convert array to a string */
-extern char *ToString(bit_arr_t *arr);
+extern char *ToString(char * dest, bit_arr_t *src);
 
 #endif
