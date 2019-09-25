4604ff06eb60c8e44dcd96f2b2792fc48066edf6
daniela.goel <danigoel@gmail.com>
2019-09-25 19:39:29 +0300

    modified:   vector.c 	deleted:    ../../c/dummy.md 	deleted:    ../dummy.md 	deleted:    ../../dummy.md

--- ds/src/vector.c
+++ ds/src/vector.c
@@ -1,11 +1,30 @@
 #include <stdio.h> /* printf */
+#include <stdlib.h> /* calloc, realloc, free */
 #include <assert.h> /* assert */
 #include <stddef.h> /* size_t */
-#include <string.h> /* strlen */
+#include <string.h> /* memset */
 
 #include "vector.h" /* vector_t */
 
-vector_t *VectorCreate(size_t element_size, size_t size);
+struct vector
+{
+	size_t ele_size;
+	void *buffer;
+	size_t capacity;
+	size_t size; /* amount of current number of elements in the array */
+};
+
+vector_t *VectorCreate(size_t element_size, size_t size)
+{
+	vector_t *vector_arr = NULL;
+	
+	vector_arr = malloc(sizeof(vector_t));
+	vector_arr->capacity = size;
+    vector_arr->size = 0;
+    vector_arr->buffer = calloc(element_size, vector_arr->size * vector_arr->capacity);
+    
+    return vector_arr;
+}
 
 void VectorDestroy(vector_t *vector_ptr);
 
