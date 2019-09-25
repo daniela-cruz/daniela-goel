881bfdbee856badffd8403dfbdace7e09bf3bec7
daniela.goel <danigoel@gmail.com>
2019-09-25 15:14:34 +0300

    modified:   stack.c

--- ds/src/stack.c
+++ ds/src/stack.c
@@ -9,81 +9,47 @@
 struct stack
 {
 	size_t ele_size;
-	size_t max_top; /* element size multiplied by amount */
 	int top; /* stack pointer, points to top of stack */
 	void *elements;
 };
 
 stack_t *StkCreate(size_t stack_size, size_t element_capacity)
 {
-	stack_t *pt = (stack_t*)malloc(sizeof(element_capacity * stack_size));
+	stack_t *stack_ptr = malloc(sizeof(stack_t));
+	stack_ptr->ele_size = element_capacity;
+    stack_ptr->top = 0;
+    stack_ptr->elements = calloc(element_capacity, stack_size);
  
-    pt->max_top = stack_size;
-    pt->top = 0;
-    pt->elements = (void *)malloc(sizeof(element_capacity));
- 
-    return pt;
+    return stack_ptr;
 }
 
-void StkPop(stack_t *element_ptr)
+void StkPop(stack_t *stk)
 {
-	char *element_ptr_cpy = NULL;
-	
-	if (StkIsEmpty(element_ptr))
-	{
-		printf("Stack is empty\n"); 
-	}
-	
-	element_ptr->top--;
-	*element_ptr_cpy = *(char *)element_ptr->elements * element_ptr->top--;
+	stk->top--;
 }
 
-void StkPush(stack_t *element_ptr, void *data)
+void StkPush(stack_t *stk, void *data)
 {
-	 char *element_ptr_cpy = NULL;
-	 
-	 assert(data);
-	 
-	 if (StkIsEmpty(element_ptr))
-    {
-        printf("Stack is empty\n"); 
-    }
-    else
-    {
-		element_ptr_cpy = (char *)element_ptr->elements + (element_ptr->top * element_ptr->ele_size);
-		element_ptr->elements = (void *)element_ptr_cpy;
-		element_ptr->elements = memcpy(element_ptr->elements, data, element_ptr->ele_size);
-		element_ptr->top++;
-	}
+	memcpy((char *)stk->elements + (stk->top * stk->ele_size), data, stk->ele_size);
+	stk->top++;
 }
 
-void *StkPeek(const stack_t *element_ptr)
+void *StkPeek(const stack_t *stk)
 {
-	char *element_ptr_cpy = NULL;
-	
-	if (!StkIsEmpty(element_ptr))
-	{
-		element_ptr_cpy = (char *)element_ptr->elements + element_ptr->top;
-		
-		return (void *)element_ptr_cpy;
-	}
-	else
-	{
-		exit(EXIT_FAILURE);
-	}
+	return (char *)stk->elements + ((stk->top-1) * stk->ele_size);
 }
 
-size_t StkCount(const stack_t *element_ptr)
+size_t StkCount(const stack_t *stk)
 {
-	return element_ptr->top;
+	return stk->top;
 }
 
-int StkIsEmpty(const stack_t *element_ptr)
+int StkIsEmpty(const stack_t *stk)
 {
-	 return (element_ptr->top == 0) ? 1: 0;
+	 return stk->top == 0;
 }
 
-void StkDestroy(stack_t *element_ptr)
+void StkDestroy(stack_t *stk)
 {	
-	free(element_ptr); element_ptr = NULL;
+	free(stk); stk = NULL;
 }
