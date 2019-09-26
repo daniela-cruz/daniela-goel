7d7220c9392bcc82ed933e0ece8e680f757247a1
daniela.goel <danigoel@gmail.com>
2019-09-26 10:52:02 +0300

    modified:   vector.c 	modified:   vector_test.c

--- ds/src/vector.c
+++ ds/src/vector.c
@@ -9,31 +9,62 @@
 struct vector
 {
 	size_t ele_size;
-	void *buffer;
+	void *data;
 	size_t capacity;
 	size_t size; /* amount of current number of elements in the array */
 };
+
+enum status_t {SUCCESS, FAILURE};
+
+const size_t capacity_factor = 2;
 
 vector_t *VectorCreate(size_t element_size, size_t size)
 {
 	vector_t *vector_arr = NULL;
 	
 	vector_arr = malloc(sizeof(vector_t));
+	vector_arr->ele_size = element_size;
+    vector_arr->data = calloc(vector_arr->ele_size, vector_arr->size * vector_arr->capacity);
 	vector_arr->capacity = size;
     vector_arr->size = 0;
-    vector_arr->buffer = calloc(element_size, vector_arr->size * vector_arr->capacity);
     
     return vector_arr;
 }
 
-void VectorDestroy(vector_t *vector_ptr);
+void VectorDestroy(vector_t *vector_ptr)
+{
+	free(vector_ptr->data); vector_ptr->data = NULL;
+	free(vector_ptr); vector_ptr = NULL;
+}
 
 void *VectorGetItemAddress(vector_t *vector_ptr, size_t index)
 {
-        return (char *)vector_ptr->buffer + index;
+        
+        return (char *)vector_ptr->data + (index * vector_ptr->ele_size);
 }
 
-int VectorPushBack(vector_t *vector_ptr, const void *data);
+int VectorPushBack(vector_t *vector_ptr, const void *new_data)
+{
+	if (vector_ptr->capacity == vector_ptr->size) /* if no more room left for new data*/
+	{
+		vector_ptr = realloc(vector_ptr->data, 
+								   (vector_ptr->ele_size) * (vector_ptr->capacity * capacity_factor));
+		vector_ptr->capacity *= capacity_factor;
+	}
+
+	if (!vector_ptr)
+	{	
+		perror("Push failed.");
+		
+		return FAILURE;
+	}
+
+	memcpy((char *)vector_ptr->data + (vector_ptr->ele_size * vector_ptr->size), 
+				 new_data, vector_ptr->ele_size);
+	vector_ptr->size++;
+	
+	return SUCCESS;
+}
 
 int VectorPopBack(vector_t * vector_ptr);
 
@@ -47,4 +78,17 @@ size_t VectorCapacity(const vector_t *vector_ptr)
 	return vector_ptr->capacity;
 }
 
-int VectorReserve(const vector_t *vector_ptr, size_t new_capacity);
+int VectorReserve(const vector_t *vector_ptr, size_t new_capacity)
+{
+	if (new_capacity > vector_ptr->capacity)
+	{
+		vector_ptr->capacity = new_capacity;
+	}
+	
+	if (vector_ptr->capacity == vector_ptr->size) /* if no more room left for new data*/
+	{
+		vector_ptr = realloc(vector_ptr->data, 
+						(vector_ptr->ele_size) * (vector_ptr->capacity * capacity_factor));
+		vector_ptr->capacity *= capacity_factor;
+	}
+}
--- ds/src/vector_test.c
+++ ds/src/vector_test.c
@@ -4,7 +4,65 @@
 
 #include "vector.h" /* vector_t */
 
+static void VectorCreateTest();
+static vector_t *PushTest(vector_t *ptr, int *new_data);
+
 int main()
 {
+	VectorCreateTest();
+	
 	return 0;
+}
+
+static void VectorCreateTest()
+{
+	vector_t *vector_ptr = NULL;
+	size_t ele_size = sizeof(int), size = 3;
+	int data_int = 123, retreived_data = 0;
+	
+	vector_ptr = VectorCreate(ele_size, size);
+	
+	printf("\nCreate test. . . \n");
+	
+	if (vector_ptr)
+	{
+		printf("SUCCESS! A pointer was allocated.\n");
+	}
+	else
+	{
+		printf("Failure! Pointer was not allocaed\n");
+	}
+	
+	PushTest(vector_ptr, &data_int);
+	
+	retreived_data = *(int *)VectorGetItemAddress(vector_ptr, 1);
+	printf("\nGet item test: %d\n", retreived_data);
+	
+	printf("\nDestroy test. . . \n");
+	VectorDestroy(vector_ptr);
+	
+	if (NULL == vector_ptr)
+	{
+		printf("SUCCESS! The pointer was freed.\n");
+	}
+	else
+	{
+		printf("Failure! Pointer was not freed\n");
+	}
+}
+
+static vector_t *PushTest(vector_t *ptr, int *new_data)
+{
+	printf("\nPush to vector test. . . \n");
+	
+	if (!VectorPushBack(ptr, new_data))
+	{
+		printf("SUCCESS! New data was pushed.\n");
+	}
+	else
+	{
+		printf("Failure! Could not push data\n");
+	}
+	
+	return ptr;
 }
