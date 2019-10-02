b89beb4e12c5214b25f0ad96745c7e65a2ce03bb
daniela.goel <danigoel@gmail.com>
2019-10-02 09:03:46 +0300

    modified:   stack.h

--- ds/src/stack.h
+++ ds/src/stack.h
@@ -5,22 +5,23 @@
 
 typedef struct stack stack_t;
 
-/* creates a stack of <size> members
-   recieves number of members that the stack can fill
-   and the size of the argument the stack is expected
-   to save. 										   */
+/* 		creates a stack of <size> members
+	   recieves number of members that the stack can fill
+	   and the size of the argument the stack is expected
+	   to save. 																		   		*/
 stack_t *StkCreate(size_t stack_size, size_t element_capacity);
 
-/* Pops the last inserted member in the stack
-   pop when stack is empty returns NULL */
+/* 		Pops the last inserted member in the stack
+   		pop when stack is empty behavior is undefined. 
+   		Complexity: O(1) 																*/
 void StkPop(stack_t *element_ptr);
 
-/* Push the requested value into the stack.
-   push when stack is full is undefined.	 		   */
+/* 		Push the requested value into the stack.
+   		push when stack is full is undefined. Complexity: O(1)	 		   */
 void  StkPush(stack_t *element_ptr, void *data);
 
-/* Peeks at the last inserted member in the stack
-   returns it to the user.							  			   */
+/* 		Peeks at the last inserted member in the stack
+   		returns it to the user. If stack is empty behavior is undefined		*/
 void *StkPeek(const stack_t *element_ptr);
 
 /* Counts the inserted member in the stack
