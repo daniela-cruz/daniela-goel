26a1dd5c89dc084e11ea53fc75d528605e76b40c
daniela.goel <danigoel@gmail.com>
2019-10-26 01:08:57 +0300

    modified:   sched.c 	renamed:    test_2.c -> sched_test.c

--- ds/src/sched/sched.c
+++ ds/src/sched/sched.c
@@ -116,37 +116,6 @@ void SchedDestroy(sched_t *scheduler)
 }
 
 /***********************************
-* 	INTERNAL FUNCTIONS 	*
-***********************************/
-
-/************TIME**********************/
-/*void TimeExeUpdate(sched_task_t *task_running)
-{
-	assert(NULL != task_running);
-	
-	task_running->execute_time = time(NULL) + task_running->interval;
-}
-
-time_t TimeOfExe(sched_task_t *task)
-{
-	assert(NULL != task);
-	
-	return task->execute_time;
-}
-
-int TimeIsBefore(void *new_task, void *first_task, void *param)
-{
-	(void)param;
-	
-	return ((sched_task_t *)new_task)->execute_time < ((sched_task_t *)first_task)->execute_time;
-}
-
-int TimeDifference(sched_task_t *task)
-{
-	return (int)((int)task->execute_time - (int)time(NULL));
-}*/
-
-/***********************************
 * 		DBUG FUNCTIONS 	*
 ***********************************/
  
