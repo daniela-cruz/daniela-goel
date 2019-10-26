a4d254adf6cf6c3ad2c618254945a27465bdbe7a
daniela.goel <danigoel@gmail.com>
2019-10-26 15:49:24 +0300

    deleted:    dll/dll_b.c 	modified:   sched/uid.c 	modified:   sched/uid.h

--- ds/src/sched/uid.c
+++ ds/src/sched/uid.c
@@ -1,7 +1,4 @@
-#include <stddef.h> /* size_t */
-#include <sys/types.h> /* pid_t */
 #include <unistd.h> /* handle sleep() */
-#include <time.h> /* time_t */
 
 #include "uid.h"
 
@@ -20,25 +17,14 @@ ilrd_uid_t UIDCreate()
 
 int UIDIsEqual(ilrd_uid_t uid1, ilrd_uid_t uid2)
 {
-	if ((uid1.curr_time == uid2.curr_time) && 
+	return ((uid1.curr_time == uid2.curr_time) && 
 		(uid1.pid == uid2.pid) && 
-		(uid1.counter == uid2.counter))
-	{
-		return 1;
-	}
-	
-	return 0;
+		(uid1.counter == uid2.counter));
 }
 
 int UIDIsError(ilrd_uid_t uid)
 {
-	if ((0 == uid.curr_time) || 
-		(0 == uid.pid))
-	{
-		return 1;
-	}
-	
-	return 0;
+	return ((0 == uid.curr_time) || (0 == uid.pid));
 }
 
 ilrd_uid_t UIDError()
