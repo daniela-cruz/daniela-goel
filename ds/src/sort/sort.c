ab23ac85d6e3583d4d0fea7977f1d36b00a0b3b6
daniela.goel <danigoel@gmail.com>
2019-11-06 18:30:49 +0200

    modified:   sort.c

--- ds/src/sort/sort.c
+++ ds/src/sort/sort.c
@@ -2,6 +2,9 @@
 #include <stddef.h> /* size_t */
 #include <assert.h> /* assert */
 
+static const size_t bits_num = sizeof(size_t) * __CHAR_BIT__;
+/*static const size_t base = 10;*/
+
 static void Swap(int *data1, int *data2);
 
 void OptimizedBubbleSort(int *arr, size_t size)
@@ -58,17 +61,17 @@ void InsertionSort(int *arr, size_t size)
             }
         }
     }*/
-    for ( i = 1; i < size; i++)
+    /*for ( i = 1; i < size; i++)
     {
         key = arr[i];
 
         if (j = i - 1; (j > 0) && (arr[i] < arr[j]); j--)
         {
-            /* rotate */
+            
 
         }
        
-    }
+    }*/
     
 }
 
@@ -159,6 +162,49 @@ int OptimizedCountSort(const int *arr, size_t size, int lower_limit,
     
     return 0;
 }
+/*
+void RadixSort(int *arr, size_t size, int lower_limit, int upper_limit, 
+    int* result)
+{
+    int denominator = 1;
+
+    assert(NULL != arr);
+    assert(NULL != result);
+
+    for (; upper_limit; )
+    {
+        RadixCountSort(arr, size, lower_limit, upper_limit, denominator, result);
+        denominator *= base;
+        upper_limit /= base;
+    }
+}*/
+
+int RadixSort(size_t *arr, size_t size, size_t n_bits)
+{
+    size_t *histogram = NULL;
+    int i = 0, j = 0;
+
+    assert(NULL != arr);
+    histogram = calloc(sizeof(*histogram), n_bits * size_t);
+    if (NULL == histogram)
+    {
+        return 1;
+    }
+    
+    for ( i = bits_num / n_bits; 0 < i; i >>= n_bits)
+    {
+        for (j = 0; j < size; j++)
+        {
+            if (i == (arr[j] & i))
+            {
+                histogram[i]++;
+            }
+        }
+    }
+    
+
+    return 0;
+}
 
 /********************
  * INTERNAL FUNCS:  *
@@ -173,4 +219,25 @@ static void Swap(int *data1, int *data2)
     temp = *data1;
     *data1 = *data2;
     *data2 = temp;
+}
+
+static void RadixCountSort(int *arr, size_t size, int lower_limit, 
+    int upper_limit, int denominator, int *result)
+{
+    int i = 0;
+
+    /*int i, freq[range]={0};
+    int output[n];
+
+    for(i=0;i<n;i++)
+            freq[(arr[i]/place)%range]++;
+    for(i=1;i<range;i++)
+            freq[i]+=freq[i-1];
+    for(i=n-1;i>=0;i--)
+    {
+            output[freq[(arr[i]/place)%range]-1]=arr[i];
+            freq[(arr[i]/place)%range]--;
+    }
+    for(i=0;i<n;i++)
+        arr[i]=output[i];*/
 }
