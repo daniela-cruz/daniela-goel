6b09cf28a3e6b4f3ba7e465a345d7b0f4c523d53
daniela.goel <danigoel@gmail.com>
2019-11-13 09:07:09 +0200

    modified:   str_rec.c

--- ds/src/str_rec/str_rec.c
+++ ds/src/str_rec/str_rec.c
@@ -1,16 +1,20 @@
 #include <stdio.h> /* printf*/
 #include <stdlib.h> /* malloc free */
 
-char *str = "hello";
+char dest[] = "hello, blah";
+char src[] = "Hey";
 
 static size_t RecStrLen(char *str);
+void RecStrCpy(char *dest, char *src);
 
 int main()
 {
-    size_t  len = 0;
+    /*size_t  len = 0;
 
     len = RecStrLen(str);
-    printf("size is: %ld\n", len);
+    printf("size is: %ld\n", len);*/
+    RecStrCpy(dest, src);
+    printf("new string is: %s\n", dest);
 
     return 0;
 }
@@ -23,7 +27,20 @@ static size_t RecStrLen(char *str)
     }
     else 
     {
-        return 1 + RecStrLen(str++);
+        return 1 + RecStrLen(str + 1);
     }
-    
+}
+
+void RecStrCpy(char *dest, char *src)
+{
+	*dest = *src;
+	
+	if ('\0' == *src) 
+	{
+		dest--;
+        *dest = '\0';
+        return; 
+	}
+	
+	RecStrCpy(src + 1, dest + 1);
 }
