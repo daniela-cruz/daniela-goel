3a85efbe3d8eb5f6c5fb7bb57e89a207925ca3f0
daniela.goel <danigoel@gmail.com>
2019-11-17 21:10:20 +0200

    modified:   str_rec.c

--- ds/src/str_rec/str_rec.c
+++ ds/src/str_rec/str_rec.c
@@ -1,9 +1,15 @@
 #include <stdio.h> /* printf*/
 #include <stdlib.h> /* malloc free */
 
+#include "stack.h" /* */
+
 int RecFibonacci(int num);
 static size_t RecStrLen(char *str);
 void RecStrCpy(char *dest, char *src);
+stack_t *RecStackSort(stack_t *stk);
+
+void RecStkSortTest(size_t size);
+void PrintArr(int *array, size_t size);
 
 char dest[] = "hello, blah";
 char src[] = "Hey";
@@ -14,7 +20,7 @@ int main()
     int num = 3;
 
     
-    printf("Fibonacci of %d is: %d\n", num, RecFibonacci(num));
+    /*printf("Fibonacci of %d is: %d\n", num, RecFibonacci(num));*/
 
     /*len = RecStrLen(str);
     printf("size is: %ld\n", len);*/
@@ -22,12 +28,16 @@ int main()
     /*RecStrCpy(dest, src);
     printf("new string is: %s\n", dest);*/
 
+    RecStkSortTest(5);
+
     return 0;
 }
 
 /********************
  *  FUNCTIONS       *
 ********************/
+
+
 int RecFibonacci(int num)
 {
     static size_t num0 = 0, num1 = 1;
@@ -86,27 +96,126 @@ int RecStrCmp(const char* s1, const char* s2)
 	return RecStrCmp(str1, str2);
 }
 
-char* StrCatRec(char* dest, const char* src)
+const char* RecStrStr(const char* dest, const char* src)
 {
-	char* d = NULL, * s = NULL;
-	
-	d = dest;
-	s = (char*)src;
+	int i = 0;
 
-	if (d + StrLen(dest) == d)
+    if (*src == '\0')
+		return dest;
+
+	for (i = 0; i < RecStrLen((char* )dest); i++)
 	{
-		*d = *s;
-
-		if ('\0' == *s)
+		if (*(dest + i) == *src) 
 		{
-			return d;
+			char* ptr = (char* )RecStrStr((char* )dest + i + 1, src + 1);
+			return (NULL != ptr) ? ptr - 1 : NULL;
 		}
-
-		return StrCatRec(++d, ++s) - 1;
 	}
 
-	d++;
-
-	return StrCatRec(d, s) - 1;
+	return NULL;
 }
+/*
+sll_node_t* RecReverse(sll_node_t* node)
+    {
+        static sll_node_t *head = NULL;
+        sll_node_t *node1 = NULL;
+        static int counter = 0;
 
+        if (0 == counter)
+        {
+            head = node;
+            counter++;
+        }
+        
+        if (node == NULL)
+        {
+            return NULL;
+        }
+
+        if (node->next == NULL) 
+        {
+            head = node;
+            return node;
+        }
+
+        node1 = RecReverse(node->next);
+        node1->next = node;
+        node->next = NULL;
+        
+        return node;
+    }*/
+
+  stack_t *RecStackSort(stack_t *stk)
+  {
+    int num1, num2;
+
+    if (1 == StkCount(stk))
+    {
+        return stk;
+    }
+    
+    num1 = *(int *)StkPeek(stk); StkPop(stk);
+    RecStackSort(stk);
+    num2 = *(int *)StkPeek(stk);  
+
+    if (num1 > num2)
+    {
+        StkPop(stk);
+        StkPush(stk, &num1);
+        StkPush(stk, &num2);
+    }
+    else
+    {
+        StkPush(stk, &num1);
+    }
+
+    return stk;
+  }  
+
+/********************
+ *  Internal funcs *
+********************/ 
+void RecStkSortTest(size_t size)
+{
+    stack_t *stk = NULL;
+    int i = 0;
+    int arr[5] = {73, 1, -7, 2, 0};
+
+    stk = StkCreate(size, sizeof(int));
+
+    for ( i = 0; i < size; i++)
+    {
+        StkPush(stk, &arr[i]);
+    }
+
+    printf("Peek shows: %d\n", *(int*)StkPeek(stk));
+    /*PrintArr(arr, size);*/
+    printf("Printing array:\n");
+
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d\n", *(arr + i));
+    }
+
+    RecStackSort(stk);
+    /*PrintArr(arr, size);*/
+     printf("Printing array:\n");
+
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d\n", *(arr + i));
+    }
+
+}
+/*
+void PrintArr(int *array, size_t size)
+{
+    int i = 0;
+
+    printf("Printing array:\n");
+
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d\n", *(array + i));
+    }
+}*/
