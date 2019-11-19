bc1e5e780a0ef84765f0632c61f5f24bcc49e4b5
daniela.goel <danigoel@gmail.com>
2019-11-19 15:39:06 +0200

    new file:   Makefile 	modified:   avl.c 	modified:   avl.h

--- ds/src/avl/avl.c
+++ ds/src/avl/avl.c
@@ -10,13 +10,42 @@ typedef struct
 struct avl 
 {
     void *tree;
-    avl_is_before_func_t is_before;
+    avl_cmp_func_t is_before;
     void *param;
 };
+
+/*---Forward declarations---*/
+static avl_node_t *NodeCreate(void *data);
 
 /**********************
  *  FUNCTIONS         *
 **********************/
+avl_t *AVLCreate(avl_cmp_func_t func, void *param)
+{
+   avl_t *tree = NULL;
+
+   return tree; 
+}
+ 
+void AVLDestroy(avl_t *tree);
+
+int AVLInsert(avl_t *tree, void *data);
+
+void AVLRemove(avl_t *tree, const void *data);
+
+/*  returns a boolean value, 1 if empty
+ *  0 if not                                     */
+int AVLIsEmpty(const avl_t *tree);
+
+size_t AVLHeight(const avl_t *tree);
+
+size_t AVLSize(const avl_t *tree);
+
+/*  return value is boolean and thus the user
+ *  should provide a suitable function          */
+int AVLForEach(avl_t *tree, avl_action_func_t func, void *param);
+
+void *AVLFind(const avl_t *tree, const void *data);
 
 /**********************
  *  ROTATORS          *
@@ -33,3 +62,20 @@ void *GetMaxNode(avl_node_t *head);
 void *GetMinNode(avl_node_t *head);
 void *GetLOCALMaxNode(avl_node_t *head);
 void *GetLOCALMinNode(avl_node_t *head);
+
+static avl_node_t *NodeCreate(void *data)
+{
+    avl_node_t *node = NULL;
+
+    node = malloc(sizeof(* node));
+    if (NULL == node)
+    {
+        return NULL;
+    }
+
+    node->data = data;
+    node->before = NULL;
+    node->after = NULL;
+
+    return node; 
+}
--- ds/src/avl/avl.h
+++ ds/src/avl/avl.h
@@ -3,8 +3,14 @@
 
 #include <stddef.h> /* size_t */
 
-typedef int (*avl_is_before_func_t)(void *data1, void *data2, void *param);
+/*  the user should provide a comparisson function
+ *  which returns a zero if data is equal, 0< if 
+ * data1 contains a higher value than data2 and 
+ * 0> vice versa                                  */
+typedef int (*avl_cmp_func_t)(const void *data1, const void *data2, void *param);
 
+/*  returns a boolean value, 0 upon success and
+ *  1 upon failure                                  */
 typedef int (*avl_action_func_t)(void *data, void *param);
 
 typedef struct avl avl_t;
@@ -12,22 +18,26 @@ typedef struct avl avl_t;
 /**********************
  *  FUNCTIONS         *
 **********************/
-avl_t *AVLCreate(avl_is_before_func_t func, void *param);
+avl_t *AVLCreate(avl_cmp_func_t func, void *param);
  
 void AVLDestroy(avl_t *tree);
 
 int AVLInsert(avl_t *tree, void *data);
 
-void AVLRemove(avl_t *tree);
+void AVLRemove(avl_t *tree, const void *data);
 
-int AVLIsEmpty(avl_t *tree);
+/*  returns a boolean value, 1 if empty
+ *  0 if not                                     */
+int AVLIsEmpty(const avl_t *tree);
 
-int AVLHeight(avl_t *tree);
+size_t AVLHeight(const avl_t *tree);
 
-size_t AVLSize(avl_t *tree);
+size_t AVLSize(const avl_t *tree);
 
+/*  return value is boolean and thus the user
+ *  should provide a suitable function          */
 int AVLForEach(avl_t *tree, avl_action_func_t func, void *param);
 
-void *AVLFind(const avl_t *tree, void *key);
+void *AVLFind(const avl_t *tree, const void *data);
 
 #endif /* __AVL_H__ */
