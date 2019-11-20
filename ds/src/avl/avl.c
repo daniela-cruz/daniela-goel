ff6b466cbcca2d5c4440f1e4d7f4c3e288c24d96
daniela.goel <danigoel@gmail.com>
2019-11-20 12:34:22 +0200

    modified:   avl.c

--- ds/src/avl/avl.c
+++ ds/src/avl/avl.c
@@ -88,7 +88,10 @@ int AVLInsert(avl_t *tree, void *data)
     return InsertRec(tree->root, tree->is_before, data);
 }
 
-void AVLRemove(avl_t *tree, const void *data);
+void AVLRemove(avl_t *tree, const void *data)
+{
+    RemoveNode(tree->root, tree->is_before, data);
+}
 
 int AVLIsEmpty(const avl_t *tree)
 {
@@ -101,7 +104,7 @@ size_t AVLHeight(const avl_t *tree)
 }
 
 size_t AVLSize(const avl_t *tree)
-{
+{ 
     return Counter(tree->root, 0);
 }
 
@@ -123,7 +126,7 @@ avl_node_t *RotateLeftLeft(avl_node_t *head);
 avl_node_t *RotateRightLeft(avl_node_t *head);
 avl_node_t *RotateRightRight(avl_node_t *head);
 
-avl_node_t *RotateLeftRight(avl_node_t *head)
+/* avl_node_t *RotateLeftRight(avl_node_t *head)
 {
     avl_node_t *new_head;
 
@@ -171,7 +174,7 @@ node * RL(node *T)
 T->right=rotateright(T->right);
 T=rotateleft(T);
 return(T);
-}
+} */
 
 /**********************
  *  Utilities         *
@@ -181,7 +184,7 @@ void *GetMinNode(avl_node_t *head);
 void *GetLOCALMaxNode(avl_node_t *head);
 void *GetLOCALMinNode(avl_node_t *head);
 
-size_t GetHeight(avl_node_t *node)
+/* size_t GetHeight(avl_node_t *node)
 {
     size_t left_height,right_height;
     
@@ -206,7 +209,7 @@ size_t GetHeight(avl_node_t *node)
         {
             right_height = 1 + node->child[AFTER]->height;
         }
-    }
+    } 
 
     if(left_height > right_height)
     {
@@ -214,7 +217,7 @@ size_t GetHeight(avl_node_t *node)
     }
 
     return right_height;
-}
+}*/
 
 int InsertRec(avl_node_t *head, avl_is_before_t func, void *data)
 {
@@ -235,6 +238,15 @@ int InsertRec(avl_node_t *head, avl_is_before_t func, void *data)
     head->height = HeightBalance(head);
 
     return status;
+}
+
+void RemoveNode(avl_node_t *head, avl_is_before_t func, void *data)
+{
+    if (func(head->data, data, NULL))
+    {
+        /* code */
+    }
+    
 }
 
 size_t HeightBalance(avl_node_t *head)
